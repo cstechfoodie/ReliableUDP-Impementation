a15840f814481cbee5a30d7fa04272af0134f709
Shunyu Wang <w_shunyu@live.concordia.ca>
2018-11-18 00:16:06 -0500

    implement reliable udp client for post method

--- HttpClient/src/main/java/ca/concordia/httpClient/lib/ReliableUDP.java
+++ HttpClient/src/main/java/ca/concordia/httpClient/lib/ReliableUDP.java
@@ -3,6 +3,7 @@ package ca.concordia.httpClient.lib;
 import static java.nio.channels.SelectionKey.OP_READ;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
@@ -11,107 +12,147 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.spi.SelectorProvider;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class ReliableUDP {
-	
-  private static final Logger logger = LoggerFactory.getLogger(ReliableUDP.class);
-	
-  public boolean handShakeDone = false;
-  
-  private static int[] acks;
-  
-  private static Packet[] spackets;
-  
-  private static Packet[] rpackets;
-  
-  
-  
-  public static int byteSize(String payload) {
-	  return payload.getBytes().length;
-  }
-  
-  private static int packetNumber(int totalPayloadSize) {
-	  return totalPayloadSize/Packet.PAYLOAD_LEN + 1;
-  }
-  
-  private int packetNumber(byte[] payload) {
-	  return payload.length/Packet.PAYLOAD_LEN + 1;
-  }
-  
-  public static String sendAndReceiveForPost(String payload, SocketAddress routerAddr, InetSocketAddress serverAddr) {
-	  try(DatagramChannel channel = DatagramChannel.open()){
-          String msg = "Hello World";
-          
-          int packetnumber = packetNumber(byteSize(payload) + 2); // including HandShake
-          acks = new int[packetnumber];
-          rpackets = new Packet[packetnumber];
-          spackets = new Packet[packetnumber];
-          Selector selector  = Selector.open();
-          
-          
-          channel.configureBlocking(false);
-          channel.register(selector, OP_READ);
-          
-          
-          Packet p = new Packet.Builder()
-                  .setType(0)
-                  .setSequenceNumber(1L)
-                  .setPortNumber(serverAddr.getPort())
-                  .setPeerAddress(serverAddr.getAddress())
-                  .setPayload(msg.getBytes())
-                  .create();
-          channel.send(p.toBuffer(), routerAddr);
 
-          logger.info("Sending \"{}\" to router at {}", msg, routerAddr);
-          
-          SenderThread a = new SenderThread(p, channel, acks);
-          a.start();
-          //WorkerThread d = new WorkerTread(Packet, Channel...);
-          
+	private static final Logger logger = LoggerFactory.getLogger(ReliableUDP.class);
 
-          // Try to receive a packet within timeout.
-          
-          
-          logger.info("Waiting for the response");
-          selector.select(5000);
+	public boolean handShakeDone = false;
 
-          Set<SelectionKey> keys = selector.selectedKeys();
+	private static int[] acks;
 
-          if(keys.isEmpty()){
-              logger.error("No response after timeout");
-              return "";
-          }
+	private static Packet[] spackets;
 
-          // We just want a single response.
-          ByteBuffer buf = ByteBuffer.allocate(Packet.MAX_LEN);
-          SocketAddress router = channel.receive(buf);
-          buf.flip();
-          Object o = "asda";
-          Packet resp = Packet.fromBuffer(buf);
-          synchronized (o){
-        	  acks[(int) (resp.getSequenceNumber() -1)] = 1;
-          }
-          logger.info("Packet: {}", resp);
-          logger.info("Router: {}", router);
-          payload = new String(resp.getPayload(), StandardCharsets.UTF_8);
-          logger.info("Payload: {}",  payload);
+	private static Packet[] rpackets;
 
-          keys.clear();
-      } catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	private static int packetnumber;
+
+	public static int byteSize(String payload) {
+		return payload.getBytes().length;
 	}
-	  return "";
-  }
-  
-  
-  public String compileResponse() {
-	  return rpackets.toString();
-  }
-  
+
+	private static int packetNumber(int totalPayloadSize) {
+		return totalPayloadSize / Packet.PAYLOAD_LEN + 1;
+	}
+
+	private static int packetNumber(byte[] payload) {
+		return payload.length / Packet.PAYLOAD_LEN + 1;
+	}
+
+	public static String sendAndReceiveForPost(String payload, SocketAddress routerAddr, InetSocketAddress serverAddr) {
+		try {
+			byte[] payloadBytes = payload.getBytes("UTF-8");
+			int payloadLen = payloadBytes.length;
+			int packetnumber = packetNumber(payloadBytes); // including HandShake
+			
+			
+			acks = new int[packetnumber];
+			rpackets = new Packet[packetnumber];
+			spackets = new Packet[packetnumber];
+			
+			
+			createPackets(payload, packetnumber, routerAddr, serverAddr);
+			
+			//handshake
+//			Packet p = new Packet.Builder().setType(0).setSequenceNumber(1L).setPortNumber(serverAddr.getPort())
+//					.setPeerAddress(serverAddr.getAddress()).setPayload("Hi".getBytes()).create();
+//			
+//			p = new Packet.Builder().setType(0).setSequenceNumber(1L).setPortNumber(serverAddr.getPort())
+//					.setPeerAddress(serverAddr.getAddress()).setPayload(("" + packetnumber).getBytes()).create();
+			
+			Selector selector = Selector.open();
+			for(int i = 0; i < packetnumber; i++) {
+				DatagramChannel channel = DatagramChannel.open();
+				channel.configureBlocking(false);
+				channel.connect(routerAddr);
+				channel.register(selector, OP_READ);
+				
+				SenderThread a = new SenderThread(spackets[i], channel, acks);
+				a.start();
+				logger.info("Sending \"{}\" to router at {}", payload, routerAddr);				
+			}
+
+			logger.info("Waiting for the response");
+			
+			int packetCounter = 0;
+			int timeoutTimes = 0;
+			while(true) {
+				selector.select(5000);
+				Set<SelectionKey> keys = selector.selectedKeys();
+				if (keys.isEmpty() && timeoutTimes == 3) {
+					logger.error("No response after timeout");
+					keys.clear();
+					break;
+				}
+				if(keys.isEmpty()) {
+					timeoutTimes++;
+					keys.clear();
+				} else {
+					SelectionKey[] keyArr = (SelectionKey[]) keys.toArray();
+					ByteBuffer buf = null;
+					Packet resp = null;
+					DatagramChannel channel = null;
+					Object o = new Object();
+					for(int i = 0; i < keyArr.length; i++) {
+						channel = (DatagramChannel) keyArr[i].channel();
+						buf = ByteBuffer.allocate(Packet.MAX_LEN);
+						SocketAddress router = channel.receive(buf);
+						buf.flip();
+						resp = Packet.fromBuffer(buf);
+						synchronized (o) {
+							acks[(int) (resp.getSequenceNumber() - 1)] = 1;
+						}
+						rpackets[(int) (resp.getSequenceNumber() - 1)] = resp;
+						packetCounter++;
+						if(packetCounter == packetnumber) {
+							break;
+						}
+					}
+					keys.clear();
+				}
+			}
+//			logger.info("Packet: {}", resp);
+//			logger.info("Router: {}", router);
+//			payload = new String(resp.getPayload(), StandardCharsets.UTF_8);
+//			logger.info("Payload: {}", payload);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return compileResponse();
+	}
+
+	private static String compileResponse() {
+		return rpackets.toString();
+	}
+	
+	private static void createPackets(String payload, int packetNumber,SocketAddress routerAddr, InetSocketAddress serverAddr) {
+		Packet p = null;
+		String singlePayload = "";
+		for(int i = 0; i < packetNumber; i++) {
+			if(i != packetNumber - 1) {
+				singlePayload = payload.substring(i * 1013, (i+1)*1013);				
+			}
+			else {
+				singlePayload = payload.substring(i * 1013);
+			}
+			try {
+				p = new Packet.Builder().setType(0).setSequenceNumber(1L).setPortNumber(serverAddr.getPort())
+						.setPeerAddress(serverAddr.getAddress()).setPayload(singlePayload.getBytes("UTF-8")).create();
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			spackets[i] = p;	
+		}
+	}
+
 }
--- HttpClient/src/main/java/ca/concordia/httpClient/lib/SenderThread.java
+++ HttpClient/src/main/java/ca/concordia/httpClient/lib/SenderThread.java
@@ -1,12 +1,21 @@
 package ca.concordia.httpClient.lib;
 
+import java.io.IOException;
 import java.nio.channels.DatagramChannel;
 
 public class SenderThread extends Thread {
+	
+	private Packet sentPacket;
+	
+	private DatagramChannel channel;
+	
+	private int[] acks;
 
 	
-	public SenderThread(Packet sentPacket, DatagramChannel c, int[] acks) {
-		
+	public SenderThread(Packet sentPacket, DatagramChannel channel, int[] acks) {
+		this.sentPacket = sentPacket;
+		this.channel = channel;
+		this.acks = acks;
 	}
 	
 	public void run() {
@@ -14,22 +23,25 @@ public class SenderThread extends Thread {
 		while(!isAcked) {
 			//sent through channel
 			try {
+				channel.write(sentPacket.toBuffer());
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			
-			//check received - scks = 1
-			//if true --  isAcked = true;
-			try {
-				this.join();
-			} catch (InterruptedException e) {
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			
+			if(acks[(int) sentPacket.getSequenceNumber()] == 1) {
+				isAcked = true;
+				try {
+					this.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
